6a1dd3ba6e23c97d6c12a7f0a28c6e47a0e4d740
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2024-10-12 17:57:07 +0300

    Adapted to new folder structure

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Makefile for Inomhus
-IMPLEMENTATIONS=Sista/include/sista/ANSI-Settings.cpp Sista/include/sista/border.cpp Sista/include/sista/coordinates.cpp Sista/include/sista/cursor.cpp Sista/include/sista/field.cpp Sista/include/sista/pawn.cpp
+IMPLEMENTATIONS = include/sista/ANSI-Settings.cpp include/sista/border.cpp include/sista/coordinates.cpp include/sista/cursor.cpp include/sista/field.cpp include/sista/pawn.cpp
 
 all:
 	g++ -std=c++17 -Wall -g -c $(IMPLEMENTATIONS)
--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-g++ -std=c++17 -Wall -g -c -static Sista/include/sista/ANSI-Settings.cpp Sista/include/sista/border.cpp Sista/include/sista/coordinates.cpp Sista/include/sista/cursor.cpp Sista/include/sista/field.cpp Sista/include/sista/pawn.cpp
+g++ -std=c++17 -Wall -g -c -static include/sista/ANSI-Settings.cpp include/sista/border.cpp include/sista/coordinates.cpp include/sista/cursor.cpp include/sista/field.cpp include/sista/pawn.cpp
 g++ -std=c++17 -Wall -g -c -static inomhus.cpp
 g++ -std=c++17 -Wall -g -static -o inomhus inomhus.o ANSI-Settings.o border.o coordinates.o cursor.o pawn.o field.o
 rm -f *.o
